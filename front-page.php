d19406edba221c85263629b5cdb22f2003e0868f
s7ilijavelimirov <ilija.velimirov@s7codedesign.com>
2023-06-16 12:52:36 +0200

    Change

--- front-page.php
+++ front-page.php
@@ -45,44 +45,6 @@ endif;
         <img src="<?php echo the_field('slika'); ?>" class="img-fluid">
     </div>
 </section>
-<!-- <div class="custom-slider">
-    <div class="slider-inner">
-        <?php
-        $torbe_query = new WP_Query(array(
-            'post_type' => 'product',
-            'post_status' => 'publish',
-            'posts_per_page' => -1,
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'product_cat',
-                    'field' => 'slug',
-                    'terms' => 'accessories'
-                )
-            )
-        ));
-
-        if ($torbe_query->have_posts()) :
-            while ($torbe_query->have_posts()) :
-                $torbe_query->the_post();
-                // Get product information here
-                $product_title = get_the_title();
-                $product_image = get_the_post_thumbnail_url();
-        ?>
-                <div class="slider-item">
-                    <a href="<?php the_permalink(); ?>">
-                        <img src="<?php echo $product_image; ?>" alt="<?php echo $product_title; ?>">
-                        <h3><?php echo $product_title; ?></h3>
-                    </a>
-                </div>
-        <?php
-            endwhile;
-        endif;
-        wp_reset_postdata();
-        ?>
-    </div>
-    <button class="prev-button">Previous</button>
-    <button class="next-button">Next</button>
-</div> -->
 <section class="custom-product-category">
     <div class="container">
         <?php echo do_shortcode('[custom_product_slider category="premium"]'); ?>
